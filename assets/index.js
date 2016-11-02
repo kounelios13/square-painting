5e39666a34d6e99790bca4d87fe54dee849d03b8
kounelios13 <pkounelios@gmail.com>
2016-11-02 20:54:33 +0200

    added index.css

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 function ChessBoard(table_id,cols,rows){
-
 	var self = this;
+	self.cols = cols || 20;
+	self.rows = rows || 20;
 	self.createChess = function(){
 		$(table_id+" tr:nth-child(even) td:nth-child(even)").addClass("black-tile");
 		$(table_id+" tr:nth-child(odd) td:nth-child(odd)").addClass("grey-tile");
 	};
 }
+var chess = new ChessBoard("#table",10,10);
+chess.createChess();

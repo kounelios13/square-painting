6901e3fc1722ad26756cb66479185e71ea2ccac6
kounelios13 <pkounelios@gmail.com>
2016-11-03 20:44:48 +0200

    working on ES6 class

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ function ChessBoard(table_id, cols, rows) {
 	var self = this;
 	self.cols = cols || 20;
 	self.rows = rows || 20;
-	var render = ()=> {
+	var render = () => {
 		var markup = "";
 		//$(table_id).html("<tr>");
 		for (var height = 0; height < self.rows; height++) {
@@ -12,23 +12,49 @@ function ChessBoard(table_id, cols, rows) {
 			markup += "</tr>"
 		}
 		$(table_id).html(markup);
-		$("#table").on("click", "td", function() {
+		$(table_id).on("click", "td", function() {
 			$(this).toggleClass("black-tile");
-		});	
+		});
 		return markup;
 	};
-	self.createChess = ()=> {
+	self.createChess = () => {
 		$(table_id + " tr:nth-child(even) td:nth-child(even)").addClass("black-tile");
 		$(table_id + " tr:nth-child(odd) td:nth-child(odd)").addClass("grey-tile");
 	};
-	self.clear =() =>{
+	self.clear = () => {
 		$(".black-tile").removeClass("black-tile");
 	};
-	$(function(){
+	$(function() {
 		/*
-		* Render on when dom is ready
-		*/
+		 * Render on when dom is ready
+		 */
 		render();
 	});
 }
-var chess = new ChessBoard("#table", 30, 30);
+
+class Painter {
+
+	constructor(table_id, cols, rows) {
+		if (!table_id)
+			throw new TypeError("Provide a valid table selector");
+		this.cols = cols || 20;
+		this.rows = rows || 20;
+		$(function() {
+			console.log("doc reaady");
+			var markup = "";
+			//$(table_id).html("<tr>");
+			for (var height = 0; height < self.rows; height++) {
+				markup += "<tr>";
+				for (var width = 0; width < self.cols; width++)
+					markup += "<td></td>";
+				markup += "</tr>"
+			}
+			$("#table").append(markup);
+			$("#table").on("click", "td", function() {
+				$(this).toggleClass("black-tile");
+			});
+		});
+	}
+
+}
+var chess = new Painter("#table", 30, 30);

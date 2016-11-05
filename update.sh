bdd92bc8bb5557a710289aefc1333c9308f48810
kounelios13 <pkounelios@gmail.com>
2016-11-05 21:34:26 +0200

    updated update script

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-git add "*.*"
+echo "Author:Manos Kounelakis"
 echo "give me a commit message:"
 read "msg"
+git add "*.*"
 git commit -m"$msg"
 git push
+#Finish the program but do not exit the termiall till the user presses a key
+echo "Press a key to exit"
+read
